7e0db4ee61830710f7085c327ba8b04bb0e4698d
usamaj7475 <49397195+usamaj7475@users.noreply.github.com>
2023-09-04 18:59:31 +0500

    commiting all the changes

---
+++ cypress/e2e/Backoffice testcases/Login.cy.js
@@ -0,0 +1,65 @@
+beforeEach(() => {
+  cy.on('uncaught:exception', (err) => {
+    // Check if the error message or stack trace includes the specific error you want to ignore
+    if (err.message.includes('Incorrect') || err.message.includes('User') || err.stack.includes('SpecificError')) {
+      // Log the error to the Cypress Command Log without failing the test
+      Cypress.log({
+        name: 'Error',
+        message: err.message,
+        consoleProps: () => ({
+          Error: err,
+        }),
+      });
+
+      // Return false to prevent the error from failing the test
+      return false;
+    }
+  });
+});
+
+describe('Login to Freight Broker back office', () => {
+
+
+  it('Login to back office with multiple user', () => {
+
+
+    cy.fixture('Usersdata').then((data) => {
+
+      data.forEach((userData, index) => {
+
+
+
+        cy.visit("https://staging.d2m5lpbf9n6q29.amplifyapp.com/admin")
+
+        cy.get('#basic_email').type(userData.username)
+        cy.get('#basic_password').type(userData.password)
+
+
+        cy.get('.ant-btn').click()
+
+
+
+        if (userData.username == "usama.javed@emerald-labs.com" && userData.password == "asdasdasd") {
+
+          cy.get('.text-xl').should('have.text', userData.expected)
+          cy.get('span[aria-label="logout"] ')
+            .click()
+        }
+
+        else if (userData.username == "usama.javed@emerald-labs.com" && userData.password == "dasdasdda") {
+          cy.get('.ant-alert')
+            .should('have.text', userData.expected)
+        }
+
+        else {
+          cy.get('.ant-alert').should('have.text', userData.expected)
+        }
+
+
+        if (index < data.length - 1) {
+          cy.wait(2000);
+        }
+      })
+    });
+  })
+})
